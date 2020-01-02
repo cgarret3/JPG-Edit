655aaf8ddaaf44824ade1f5a054f16bbc8f76706
cgarret3 (Chris Garrety) <chris.garrety@gmail.com>
2020-01-02 01:19:55 -0500

    Wrote a bit more for the parsing code to cover incorrect filenames etc. but am tired so I have set a marker "Not yet implemented."  Back for more tomorrow

--- src/test_jpg_edit.c
+++ src/test_jpg_edit.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include "jpg_edit.h"
 
 int main(int argc, char** argv)
@@ -7,6 +8,7 @@ int main(int argc, char** argv)
   // Parse argv
   int opt;
   char* filename = NULL;
+  char user_answer;
 
   while((opt = getopt(argc, argv, "f:")) != -1)
   {
@@ -32,13 +34,9 @@ int main(int argc, char** argv)
   // Ensure the filename is set
   if(filename == NULL)
   {
-    char user_answer = 'o';
-    while(user_answer != 'y' && user_answer != 'Y' && user_answer != 'n' && user_answer != 'N')
-    {
-      printf("\nDefault filename is 'test.jpg'\nContinue with this filename? (y/n) ");
-      scanf("%c", &user_answer);
-    }
-    if(user_answer == 'n' || user_answer == 'N')
+    printf("\nNo filename specified. Set output filename to 'test.jpg'? (y/n) ");
+    scanf("%c", &user_answer);
+    if(user_answer != 'y' && user_answer != 'Y')
     {
       printf("\nExiting...\n\n");
       return 0;
@@ -46,12 +44,56 @@ int main(int argc, char** argv)
     else
     {
       filename = "test.jpg";
+      printf("Output filename is set to '%s'\n\n", filename);
+    }
+  }
+  // Ensure the filename follows the format xxx.jpg
+  else
+  {
+    printf("\nTODO: Not yet implemented\n\n");
+    return -1;
+
+    for(int i = 0; i < strlen(filename); i++)
+    {
+      if(filename[i] == '.' && filename[i] == '\0')
+      {
+        if(
+          filename[i+1] == 'j' &&
+          filename[i+2] == 'p' &&
+          filename[i+3] == 'g' &&
+          filename[i+4] == '\0')
+        {
+          // do nothing
+        }
+        else
+        {
+          printf("\nThe filename must follow the format 'xxx.jpg'.  End this filename in '.jpg'? (y/n) ");
+          scanf("%c", &user_answer);
+          if(user_answer == 'y' || user_answer == 'Y')
+          {
+            char tmp[i+4];
+            int j;
+            for(j = 0; j < i; j++)
+            {
+              tmp[j] = filename[j];
+            }
+            tmp[j+1] = '.';
+            tmp[j+2] = 'j';
+            tmp[j+3] = 'p';
+            tmp[j+4] = 'g';
+            tmp[j+5] = '\0';
+            filename = tmp;
+            printf("The filename has been set to '%s'", filename);
+          }
+          else
+          {
+            printf("\nExiting...\n\n");
+          }
+        }
+      }
     }
   }
 
-  /*
-    TODO: Ensure filename follows the format xxx.jpg
-  */
 
 
 
